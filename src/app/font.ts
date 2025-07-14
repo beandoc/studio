09257475d8895fb2df45293e1838bb05a80de070
sachin srivastava <chiin.says@gmail.com>
2025-07-14 12:44:48 +0000

    Why the complete application is so Damm slow... Its take ages for each p

---
+++ src/app/font.ts
@@ -0,0 +1,9 @@
+
+import { PT_Sans } from "next/font/google";
+
+export const pt_sans = PT_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: '--font-sans',
+});
